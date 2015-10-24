4a2b805135df43ac99d94e75344b2fe8112759ff
Chris Wolfe <chriswwolfe@gmail.com>
2015-10-24 14:52:03 -0500

    Working on getting connected to the postgres DB

--- Dockerfile
+++ Dockerfile
@@ -19,8 +19,11 @@ RUN virtualenv /virtualenv && \
 
 RUN mkdir -p /var/www/vmprof/static
 
+ENV DJANGO_SETTINGS_MODULE=settings.docker
+
 RUN cd /var/www/vmprof/static && \
     /virtualenv/bin/python /var/www/vmprof/manage.py collectstatic -c --noinput && \
+    /virtualenv/bin/python /var/www/vmprof/manage.py syncdb && \
     /virtualenv/bin/python /var/www/vmprof/manage.py migrate && \
     /virtualenv/bin/twistd web --port 8888 --wsgi /var/www/vmprof/server/wsgi.py
 
---
+++ settings/docker.py
@@ -0,0 +1,35 @@
+from . import *
+
+
+DEBUG = True
+ALLOWED_HOSTS = ['vmprof.com']
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'postgres',
+        'PORT': 5432,
+    }
+}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, '..', 'vmprof.log'),
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
